0c2e77b71655a7f5abc7dbb7b53debd6e7714d3e
MehtaArpita <a9mehta@ucsd.edu>
2022-11-12 09:40:32 -0800

    wk5 hw updated

--- hardware/wk5/hw1_2/verilog/mac_tile.v
+++ hardware/wk5/hw1_2/verilog/mac_tile.v
@@ -14,8 +14,35 @@ input  [psum_bw-1:0] in_n;
 input  clk;
 input  reset;
 
-...
-...
+reg [1:0] inst_q
+reg [bw-1:0] a_q;
+reg [bw-1:0] b_q;
+reg [psum_bw-1:0] c_q;
+reg load_ready_q;
+
+always @(posedge clk) begin
+	if (rst) begin
+		// reset
+		inst_q <= 2'b0;
+		load_ready_q <= 1'b1;
+		
+	end
+	else begin
+		inst_q[1] <= inst_w[1];
+
+		if (inst_w[1] && load_ready_q) begin
+			b_q <= in_w;
+			load_ready_q <= 1'b0;
+		end 
+		else if (~load_ready_q) begin 
+			inst_q[0] <= inst_w[0];
+		end 
+
+		if (inst_w != 2'b0) begin 
+			a_q <= in_w;
+		end 
+	end
+end
 
 mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
         .a(a_q), 
@@ -24,7 +51,11 @@ mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
 	.out(mac_out)
 ); 
 
-...
-...
+// assign outputs 
+
+assign out_e = a_q;
+assign inst_e = inst_q;
+assign out_s = mac_out ;
+
 
 endmodule
